1e4c6a6f615df5403d5927a57567603f9f3f7fd9
Yabe-B <yeabsirabehailu92@gmail.com>
2025-12-04 14:39:08 +0300

    Implement HomePage layout with AppBar and task list display

--- taskly/lib/pages/home_page.dart
+++ taskly/lib/pages/home_page.dart
@@ -9,9 +9,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late double _deviceHeight ,  _deviceWidth;
   _HomePageState();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight * 0.15,
+        title: const Text('Taskly', style: TextStyle(fontSize: 25),),
+        centerTitle: true,
+      ),
+      body:_taskList(),
+    );
+  }
+    Widget _taskList() {
+    return ListView(
+      children: [
+         ListTile(
+          title: const Text('Task 1',style: TextStyle(decoration: TextDecoration.lineThrough),),
+          subtitle: Text(DateTime.now().toString()),
+          trailing: Icon(Icons.check_circle_outline, color: Colors.red),
+        ),
+      ],
+    );
   }
 }
